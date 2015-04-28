e1bc7d416b55ebd4dc4816d2bf82d389edc93ed4
Demengjkee <ncherepkin@live.com>
2015-04-28 04:55:01 +0300

    kinda unstable, but userlist works. TODO history saving

--- src/main/java/org/server/ChatServlet.java
+++ src/main/java/org/server/ChatServlet.java
@@ -28,9 +28,11 @@ public class ChatServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
+
         if(!sessions.contains(request.getSession())) {
             sessions.add(request.getSession());
         }
+
         response.setCharacterEncoding("UTF-8");
         final AsyncContext asyncContext = request.startAsync(request, response);
         asyncContext.setTimeout(Integer.MAX_VALUE - 1);
@@ -41,10 +43,12 @@ public class ChatServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         List<AsyncContext> asyncContexts = new ArrayList<>(this.contexts);
         this.contexts.clear();
         Message message = new Message();
         message.setType(request.getParameter("type"));
+
         if(message.getType().equals("add")) {
             message.setUsername(request.getParameter("username"));
             message.setMessage(request.getParameter("message"));
@@ -69,63 +73,30 @@ public class ChatServlet extends HttpServlet {
                 }
             }
         }
+
         if(message.getType().equals("log")) {
             request.getSession().setAttribute("username", request.getParameter("username"));
+            List<String> usernames = new ArrayList<>();
+            for(HttpSession session : sessions) {
+                usernames.add((String)session.getAttribute("username"));
+            }
+            JsonProvider jsonProvider = JsonProvider.provider();
+            System.out.println(usernames.toString());
+            JsonObject respMessage = jsonProvider.createObjectBuilder()
+                    .add("usernames", usernames.toString())
+                    .add("type", message.getType())
+                    .build();
             for(AsyncContext asyncContext : asyncContexts) {
                 try(PrintWriter writer = asyncContext.getResponse().getWriter()) {
-                    List<String> usernames = new ArrayList<>();
-                    for(HttpSession session : sessions) {
-                        usernames.add((String)session.getAttribute("username"));
-                    }
-                    JsonProvider jsonProvider = JsonProvider.provider();
-                    System.out.println(usernames.toString());
-                    JsonObject respMessage = jsonProvider.createObjectBuilder()
-                            .add("usernames", usernames.toString())
-                            .add("type", message.getType())
-                            .build();
                     writer.print(respMessage);
                     writer.flush();
+                    asyncContext.complete();
                 }
                 catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
         }
-      /*  System.out.println("it works POST");
-
-        String readerTmp;
-        Message message = new Message();
-        BufferedReader reader = request.getReader();
-        readerTmp = reader.readLine();
-        StringTokenizer st = new StringTokenizer(readerTmp, "=&");
-        st.nextToken();
-        message.setType(st.nextToken());
-        if(message.getType().equals("add")) {
-            st.nextToken();
-            message.setMessage(st.nextToken());
-            st.nextToken();
-            message.setUsername(st.nextToken());
-
-            message.setId(id);
-            messages.add(message);
-            id++;
-            JsonProvider jsonProvider = JsonProvider.provider();
-            PrintWriter writer = response.getWriter();
-            JsonObject respMessage = jsonProvider.createObjectBuilder()
-                    .add("id", message.getId())
-                    .add("username", message.getUsername())
-                    .add("message", message.getMessage())
-                    .build();
-            writer.print(respMessage);
-
-        }
-
-        if(message.getType().equals("log")) {
-            st.nextToken();
-            request.getSession().setAttribute("username", st.nextToken());
-            PrintWriter writer = response.getWriter();
-            writer.print(true);
-        }*/
     }
 
     @Override
--- src/main/webapp/js/Events.js
+++ src/main/webapp/js/Events.js
@@ -1,22 +1,6 @@
 function start() {
     var username;
     var sendButton = document.getElementsByClassName("sendButton");
-    /* $.ajax({
-     url: "http://localhost:8080/ChatServlet",
-     method: 'GET',
-     data: {
-     type: "connect"
-     }
-     })
-     .done(function(resp) {
-     var tmp = resp.split("}");
-
-
-     for(var i = 0; i < tmp.length; i++) {
-     console.log(JSON.parse(tmp[i]));
-     addMessage(JSON.stringify(JSON.parse(tmp[i])));
-     }
-     });*/
     $(function () {
         $('#chat').perfectScrollbar();
         Ps.initialize(document.getElementById('chat'));
@@ -31,8 +15,6 @@ function start() {
         + "<p class='text'>" + Text + "</p>";
         return newDiv;
     }
-
-    //TODO
     sendButton[0].addEventListener("click", function () {
         if(username !== undefined) {
             var message = document.getElementsByClassName("message")[0].value;
@@ -45,11 +27,6 @@ function start() {
                     username: username
                 }
             });
-            /*.done(function (msg) {
-             console.log(msg);
-             addMessage(msg);
-             });*/
-            //addDevice(name, message);
         }
         else{
             alert("login pls");
@@ -61,33 +38,34 @@ function start() {
             var tmp = $.parseJSON(resp);
             var mainWindow = document.getElementsByClassName("mainWindow")[0];
             var msg = tmp.message;
-            /*document.getElementsByClassName("message")[0].value;*/
             var messageDiv = addMessageDiv(msg, tmp.username);
             mainWindow.appendChild(messageDiv);
             $(messageDiv).hide().fadeIn(150);
             document.getElementsByClassName("message")[0].value = "";
-            messageDiv.addEventListener("mouseover", function () {
-                this.childNodes[1].style.visibility = "visible";
-                this.childNodes[2].style.visibility = "visible";
-            });
-            messageDiv.addEventListener("mouseout", function () {
-                this.childNodes[1].style.visibility = "hidden";
-                this.childNodes[2].style.visibility = "hidden";
-            });
-            messageDiv.childNodes[1].addEventListener("click", function () {
-                var todel = this.parentNode;
-                $(todel).fadeOut(150, function () {
-                    todel.parentNode.removeChild(todel);
+            if(tmp.username === username) {
+                messageDiv.addEventListener("mouseover", function () {
+                    this.childNodes[1].style.visibility = "visible";
+                    this.childNodes[2].style.visibility = "visible";
                 });
+                messageDiv.addEventListener("mouseout", function () {
+                    this.childNodes[1].style.visibility = "hidden";
+                    this.childNodes[2].style.visibility = "hidden";
+                });
+                messageDiv.childNodes[1].addEventListener("click", function () {
+                    var todel = this.parentNode;
+                    $(todel).fadeOut(150, function () {
+                        todel.parentNode.removeChild(todel);
+                    });
 
 
-            });
-            messageDiv.childNodes[2].addEventListener("click", function () {
-                var oldmsg = this.parentNode.childNodes[3];
-                oldmsg = oldmsg.innerHTML;
-                this.parentNode.childNodes[3].innerHTML = prompt("EditMessage", oldmsg);
-                $(this.parentNode.childNodes[3]).hide().fadeIn(300);
-            });
+                });
+                messageDiv.childNodes[2].addEventListener("click", function () {
+                    var oldmsg = this.parentNode.childNodes[3];
+                    oldmsg = oldmsg.innerHTML;
+                    this.parentNode.childNodes[3].innerHTML = prompt("EditMessage", oldmsg);
+                    $(this.parentNode.childNodes[3]).hide().fadeIn(300);
+                });
+            }
         }
         else {
             alert("please, enter your nickname");
@@ -104,24 +82,24 @@ function start() {
     var setNameButton = document.getElementsByClassName("confirmNameButton")[0];
     setNameButton.addEventListener("click", function () {
         username = document.getElementsByClassName("username")[0].value;
-        /*$.ajax({
+        $.ajax({
             url: "http://localhost:8080/ChatServlet",
             method: 'POST',
             data: {
                 type: "log",
                 username: username
             }
-        });*/
-        /* .done(function (resp) {
-         console.log(resp);
-         log(resp);
-         })*/
+        });
     });
-    //TODO fix dat shit
     function log(resp) {
 
         var user = document.getElementsByClassName("usersOnline")[0];
-
+        var oldOnlineList = document.getElementsByClassName("onlineUser");
+        if(oldOnlineList.length > 0) {
+            while (oldOnlineList.length > 0) {
+                oldOnlineList[0].remove();
+            }
+        }
         var users = JSON.parse(resp).usernames.split(",");
         for(var i = 0; i < users.length; i++) {
             var usr = users[i].replace('[','').replace(']','').replace(' ','');
@@ -145,9 +123,9 @@ function start() {
                 if (JSON.parse(resp).type === 'add') {
                     addMessage(resp);
                 }
-               /* if (JSON.parse(resp).type === 'log') {
+                if (JSON.parse(resp).type === 'log') {
                     log(resp);
-                }*/
+                }
                 var status = document.getElementsByClassName("rightText")[0];
                 status.innerHTML = "Server: OK";
             })
@@ -159,5 +137,4 @@ function start() {
                 poll();
             })
     })();
-
 }
