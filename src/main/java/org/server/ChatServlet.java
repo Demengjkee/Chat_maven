b8489289443082b4cb46444949ed9af0a9673f1b
Demengjkee <ncherepkin@live.com>
2015-05-19 21:03:14 +0300

    deleted XML, added MySQL little support

--- src/main/java/org/server/ChatServlet.java
+++ src/main/java/org/server/ChatServlet.java
@@ -1,11 +1,9 @@
 package org.server;
 
-import com.mysql.jdbc.Connection;
+
+
+
 import org.message.Message;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
@@ -13,20 +11,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.Date;
 import javax.json.*;
 import javax.json.spi.*;
 import javax.servlet.http.HttpSession;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+import java.sql.*;
+
 
 @WebServlet(urlPatterns = {"/ChatServlet"}, asyncSupported = true)
 public class ChatServlet extends HttpServlet {
@@ -35,14 +28,32 @@ public class ChatServlet extends HttpServlet {
     private final List<Message> messages = Collections.synchronizedList(new ArrayList<Message>());
     private List<AsyncContext> contexts = new LinkedList<>();
     private final List<HttpSession> sessions = Collections.synchronizedList(new ArrayList<HttpSession>());
+    private Connection con = null;
+    private PreparedStatement pst = null;
+    private Statement st = null;
+    private ResultSet rs = null;
 
     public void init() throws ServletException {
-        this.readXML();
-        for(Message message : messages) {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-YYYY HH:mm:ss");
-            System.out.println(dateFormat.format(message.getDate()) + " " + message.getUsername() + ": "
-                + message.getMessage());
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CHAT_DB", "root", "root");
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT COUNT(id) FROM messages;");
+            if(rs.next()) {
+                if(rs.getInt(1) == 0) {
+                    return;
+                }
+            }
+            rs = st.executeQuery("SELECT id FROM messages ORDER BY id DESC LIMIT 1;");
+            if(rs.next()) {
+                this.id = rs.getInt(1) + 1;
+            }
         }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+
         super.init();
     }
 
@@ -50,6 +61,8 @@ public class ChatServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
 
+
+        response.addHeader("Access-Control-Allow_Methods", "*");
         if(!sessions.contains(request.getSession())) {
             sessions.add(request.getSession());
         }
@@ -66,6 +79,7 @@ public class ChatServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        response.addHeader("Access-Control-Allow_Methods", "*");
         List<AsyncContext> asyncContexts = new ArrayList<>(this.contexts);
         this.contexts.clear();
         Message message = new Message();
@@ -76,6 +90,17 @@ public class ChatServlet extends HttpServlet {
             message.setMessage(request.getParameter("message"));
             message.setId(this.id);
             this.id++;
+            try {
+                pst = con.prepareStatement("INSERT INTO messages VALUES (?, ?, ?)");
+                pst.setInt(1, message.getId());
+                pst.setString(2, message.getUsername());
+                pst.setString(3, message.getMessage());
+                pst.executeUpdate();
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
             messages.add(message);
             JsonProvider jsonProvider = JsonProvider.provider();
             JsonObject respMessage = jsonProvider.createObjectBuilder()
@@ -127,6 +152,7 @@ public class ChatServlet extends HttpServlet {
     public void doDelete(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        response.addHeader("Access-Control-Allow_Methods", "*");
         List<AsyncContext> asyncContexts = new ArrayList<>(this.contexts);
         this.contexts.clear();
         System.out.println(123);
@@ -152,77 +178,29 @@ public class ChatServlet extends HttpServlet {
             throws ServletException, IOException {
 
     }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow_Methods", "PUT, DELETE, POST, GET, OPTIONS");
+    }
+
     public void destroy() {
-        this.writeXML();
+        try {
+            if (con != null) {
+                con.close();
+            }
+            if (pst != null) {
+                pst.close();
+            }
+            if (st != null) {
+                st.close();
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
         super.destroy();
     }
 
-    public void writeXML() {
-        try {
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.newDocument();
-            Element root = doc.createElement("messages");
-            doc.appendChild(root);
-            synchronized (messages) {
-                for (Message message : messages) {
-                    Element msg = doc.createElement("msg");
-                    root.appendChild(msg);
-                    Element id = doc.createElement("id");
-                    id.appendChild(doc.createTextNode(Integer.toString(message.getId())));
-                    msg.appendChild(id);
-                    Element username = doc.createElement("username");
-                    username.appendChild(doc.createTextNode(message.getUsername()));
-                    msg.appendChild(username);
-                    Element mes = doc.createElement("message");
-                    mes.appendChild(doc.createTextNode(message.getMessage()));
-                    msg.appendChild(mes);
-                    Element date = doc.createElement("date");
-                    SimpleDateFormat format = new SimpleDateFormat("dd-MM-YYYY HH:mm:ss");
-                    date.appendChild(doc.createTextNode(format.format(message.getDate())));
-                    msg.appendChild(date);
-                }
-            }
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(doc);
-            File f = new File("history.xml");
-            StreamResult result = new StreamResult(f);
-            transformer.transform(source, result);
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
 
-    public void readXML() {
-        File fXmlFile = new File("history.xml");
-        try {
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(fXmlFile);
-            doc.getDocumentElement().normalize();
-            NodeList nList = doc.getElementsByTagName("msg");
-            synchronized (messages) {
-                for (int i = 0; i < nList.getLength(); i++) {
-                    Node nItem = nList.item(i);
-                    if (nItem.getNodeType() == Node.ELEMENT_NODE) {
-                        Element element = (Element) nItem;
-                        Message m = new Message();
-                        m.setId(Integer.parseInt(element.getElementsByTagName("id").item(0).getTextContent()));
-                        m.setMessage(element.getElementsByTagName("message").item(0).getTextContent());
-                        m.setUsername(element.getElementsByTagName("username").item(0).getTextContent());
-                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-YYYY HH:mm:ss");
-                        m.setDate(dateFormat.parse(element.getElementsByTagName("date").item(0).getTextContent()));
-                        messages.add(m);
-                    }
-
-                }
-                id = messages.size();
-            }
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
 }
